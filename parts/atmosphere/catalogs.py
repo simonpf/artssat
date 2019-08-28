964fde5a62a63f5dc89e693ac3b20ac56f024da0
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2019-08-28 17:49:37 +0200

    Added catalogs.py module.

---
+++ parts/atmosphere/catalogs.py
@@ -0,0 +1,55 @@
+import numpy as np
+import os
+from abc import ABCMeta, abstractmethod, abstractproperty
+
+class LineCatalog(metaclass = ABCMeta):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def setup(self, workspace):
+        pass
+
+class Hitran(LineCatalog):
+    def __init__(self, path = None):
+
+        self.path = path
+        if self.path is None:
+            try:
+                self.path = os.environ["PATH"]
+            except:
+                self.path = "HITRAN2012.par"
+
+    def setup(self, workspace, sensors):
+
+        f_max = 0.0
+        f_min = np.finfo(np.float64).max
+        for s in sensors:
+            f_min = min(s.f_grid.min(), f_min)
+            f_max = max(s.f_grid.max(), f_max)
+
+        workspace.abs_linesReadFromHitran(self.path, f_min, f_max)
+
+class Perrin(LineCatalog):
+    def __init__(self, path = None):
+
+        self.path = path
+        if self.path is None:
+            try:
+                self.path = os.environ["PERRIN_PATH"]
+            except:
+                self.path = "spectroscopy/Perrin/"
+
+    def setup(self, workspace, sensors):
+
+        f_max = 0.0
+        f_min = np.finfo(np.float64).max
+        for s in sensors:
+            f_min = min(s.f_grid.min(), f_min)
+            f_max = max(s.f_grid.max(), f_max)
+
+        print(f_max / 1e9, f_min / 1e9)
+
+        workspace.abs_linesReadFromSplitArtscat(basename = self.path,
+                                                fmin = f_min,
+                                                fmax = f_max)
