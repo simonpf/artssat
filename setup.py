19059796e647823bbfcf7f8a8218b9f3771ca85c
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2020-03-26 12:01:36 +0100

    Added setup.py for installation.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='parts',  # Required
+    version='0.1',  # Required
+    description='High-level simulation and retrieval framework for microwave and IR remote sensing.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # Optional (see note above)
+    url='https://github.com/simonpf/parts',  # Optional
+    author='Simon Pfreundschuh',  # Optional
+    author_email='simon.pfreundschuh@chalmers.se',  # Optional
+    install_requires=["pyarts"],
+    packages=["parts"],
+    python_requires='>=3.6',
+    project_urls={  # Optional
+        'Source': 'https://github.com/simonpf/parts/',
+    })
