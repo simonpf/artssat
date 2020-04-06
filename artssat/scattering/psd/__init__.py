42aab5984975166cec53078111990a79420892df
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2020-04-06 20:08:09 +0200

    Added AB12 PSD.

--- artssat/scattering/psd/__init__.py
+++ artssat/scattering/psd/__init__.py
@@ -12,5 +12,6 @@ subpackage provides functionality for the handling of PSD data.
 """
 from artssat.scattering.psd.d14     import D14, D14N, D14MN
 from artssat.scattering.psd.my05    import MY05
+from artssat.scattering.psd.ab12    import AB12
 from artssat.scattering.psd.binned      import Binned
 from artssat.scattering.psd.fixed_shape import FixedShape
