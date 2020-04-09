de17eee618271d8d96adeaab55c104943ebe0ad3
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2020-04-09 10:53:33 +0200

    Added test for surface models.

---
+++ tests/surface/test_surface.py
@@ -0,0 +1,116 @@
+import numpy as np
+import pytest
+
+import artssat
+from artssat import ArtsSimulation
+from artssat.atmosphere import Atmosphere1D
+from artssat.atmosphere.absorption import O2, N2, H2O
+from artssat.scattering import ScatteringSpecies, D14
+from artssat.atmosphere.surface import Tessem, Telsem, CombinedSurface
+from artssat.sensor import CloudSat, MWI
+from pyarts.workspace.api import include_path_push
+
+include_path_push("../data")
+
+from examples.data_provider import DataProvider
+
+import matplotlib.pyplot as plt
+
+import os
+import sys
+test_path = os.path.join(os.path.dirname(artssat.__file__), "..", "tests")
+sys.path.append(test_path)
+from utils.data import scattering_data, scattering_meta
+
+
+def test_simulation_tessem():
+    atmosphere = Atmosphere1D(absorbers = [O2(), N2(), H2O()],
+                              surface = Tessem())
+    ici = ICI()
+    ici.sensor_line_of_sight = np.array([[135.0]])
+    ici.sensor_position = np.array([[600e3]])
+
+    simulation = ArtsSimulation(atmosphere = atmosphere,
+                                data_provider = DataProvider(),
+                                sensors = [ici])
+    simulation.setup()
+    simulation.run()
+
+    y1 = np.copy(ici.y)
+
+    simulation.run()
+    y2 = np.copy(ici.y)
+
+    assert(np.all(np.isclose(y1, y2)))
+
+
+@pytest.mark.skip(reason="Needs TELSEM2 atlases.")
+def test_simulation_telsem():
+    atmosphere = Atmosphere1D(absorbers = [O2(), N2(), H2O()],
+                              surface = Telsem("/home/simon/Downloads"))
+    ici = ICI()
+    ici.sensor_line_of_sight = np.array([[135.0]])
+    ici.sensor_position = np.array([[600e3]])
+
+    data_provider = DataProvider()
+    data_provider.surface_temperature = 280.0 * np.ones((1, 1))
+    data_provider.surface_latitude = 58.0
+    data_provider.surface_longitude = 12.0
+    simulation = ArtsSimulation(atmosphere = atmosphere,
+                                data_provider = data_provider,
+                                sensors = [ici])
+    simulation.setup()
+    simulation.run()
+
+@pytest.mark.skip(reason="Needs TELSEM2 atlases.")
+def test_simulation_combined():
+    surface_1 = Tessem()
+    surface_2 = Telsem("/home/simon/Downloads")
+    surface = CombinedSurface(surface_1,
+                              surface_2)
+    atmosphere = Atmosphere1D(absorbers = [O2(), N2(), H2O()],
+                              surface = surface)
+    ici = MWI()
+    ici.sensor_line_of_sight = np.array([[135.0]])
+    ici.sensor_position = np.array([[600e3]])
+
+    data_provider = DataProvider()
+    data_provider.surface_temperature = 280.0 * np.ones((1, 1))
+    data_provider.surface_latitude = 58.0
+    data_provider.surface_longitude = 12.0
+    data_provider.surface_type = 0.0
+
+    simulation = ArtsSimulation(atmosphere = atmosphere,
+                                data_provider = data_provider,
+                                sensors = [ici])
+
+    simulation.setup()
+    simulation.run()
+    y = np.copy(ici.y)
+
+    atmosphere_r = Atmosphere1D(absorbers = [O2(), N2(), H2O()],
+                                surface = surface_1)
+    simulation_r = ArtsSimulation(atmosphere = atmosphere_r,
+                                  data_provider = data_provider,
+                                  sensors = [ici])
+    simulation_r.setup()
+    simulation_r.run()
+    y_r = np.copy(ici.y)
+
+    assert(np.allclose(y, y_r))
+
+    data_provider.surface_type = 1.0
+    simulation.setup()
+    simulation.run()
+    y = np.copy(ici.y)
+
+    atmosphere_r = Atmosphere1D(absorbers = [O2(), N2(), H2O()],
+                                surface = surface_2)
+    simulation_r = ArtsSimulation(atmosphere = atmosphere_r,
+                                  data_provider = data_provider,
+                                  sensors = [ici])
+    simulation_r.setup()
+    simulation_r.run()
+    y_r = np.copy(ici.y)
+
+    assert(np.allclose(y, y_r))
