02fc656c5e781a8668e76dcea22fd84ae6d7cd29
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2020-04-16 13:24:07 +0200

    Updated dependencies.

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='artssat',  # Required
-    version='0.1',  # Required
+    version='0.1.1',  # Required
     description='High-level simulation and retrieval framework for microwave and IR remote sensing.',
     long_description=long_description,
     long_description_content_type='text/markdown',  # Optional (see note above)
     url='https://github.com/simonpf/artssat',  # Optional
     author='Simon Pfreundschuh',  # Optional
     author_email='simon.pfreundschuh@chalmers.se',  # Optional
-    install_requires=["pyarts", "typhon"],
+    install_requires=["pyarts", "typhon", "netCDF4"],
     packages=find_packages(exclude=['examples', 'doc', 'misc', 'tests']),
     python_requires='>=3.6',
     project_urls={  # Optional
